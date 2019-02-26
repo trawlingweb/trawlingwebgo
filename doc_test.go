af583caf076a0da1dcb70749039440907b60a25c
Josep Subils <js@js.gl>
2019-02-26 09:08:22 +0100

    [add] New test file

---
+++ doc_test.go
@@ -0,0 +1,18 @@
+package trawlingwebgo
+
+import (
+	"testing"
+)
+
+func TestRequest(t *testing.T) {
+	request := TrwRequest{Token: "ea58ad77426816b16f2cd3c950de07886bc64472", Query: "Girona"}
+	ret, err := Query(request)
+	if err != nil {
+		panic(err.Error())
+	} else {
+		t.Log("Articles: ", len(ret.Response.Data))
+		for _, article := range ret.Response.Data {
+			t.Log(article.URL)
+		}
+	}
+}
