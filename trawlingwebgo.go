81c51dfa36ce4e3115f0ec5a92016679fd027566
Umair Raza <axtruo@gmail.com>
2023-07-24 21:39:02 +0500

    update endpoint

--- trawlingwebgo.go
+++ trawlingwebgo.go
@@ -157,7 +157,7 @@ func SCRTweetRequest(url string) (models.TwitterScrResponse, error) {
 
 func TwitterQuery(params models.ArticleRequest) (models.TwitterScrResponse, error) {
 	values := reflect.ValueOf(params)
-	twurl := "https://twitter.trawlingweb.com/posts_full/?"
+	twurl := "https://twitter.trawlingweb.com/posts_history/?"
 	for i := 0; i < values.NumField(); i++ {
 		if values.Field(i).String() != "" {
 			if i != 0 {
